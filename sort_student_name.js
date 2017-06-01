2c0ee5d535e99008986d5f8a21f04350ad80b616
tian <tianweirandai@gmail.com>
2017-06-01 19:55:44 +0000

    complete and refactored sort by name with age

---
+++ sort_student_name.js
@@ -0,0 +1,38 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+
+// sort by name
+// function sortByName(a, b) {
+//   var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+//   var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+//   if (nameA < nameB) {
+//     return -1;
+//   }
+//   if (nameA > nameB) {
+//     return 1;
+//   }
+
+//   // names must be equal
+//   return a.age - b.age;
+// }
+
+const sortByName = (a, b) => {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return a.age - b.age;
+};
+
+console.log(students.sort(sortByName));
