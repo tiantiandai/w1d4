f5e5ebc26fce73f1469b43aa25011c4a60f92d12
tian <tianweirandai@gmail.com>
2017-06-02 00:40:02 +0000

    cheat_dice

---
+++ cheat_dice.js
@@ -0,0 +1,18 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  var index = 0;
+
+  return function() {
+    var result = list[index % list.length];
+    index++;
+    return result;
+
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
